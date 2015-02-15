ed77adb4c7a441eba384c18f61680c3ed7ffc8a5
Austin Willis <willisaj@rose-hulman.edu>
2015-02-14 22:23:24 -0500

    Creating enums

--- simulation/src/main.rs
+++ simulation/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// Messages from Actors to a Market
+enum MarketMessages {
+  SellRequest(TransactionRequest),
+  BuyRequest(TransactionRequest),
+  Commit(usize), //Id of the actor
+  Cancel(usize), //Id of the actor
+}
+
+// Messages from a Market to an Actor
+enum ActorMessages {
+  StockRequest(StockRequest),
+  MoneyRequest(usize), //The amount of money needed to buy the stock(s)
+  CommitTransaction(TransactionRequest), //The information related to the transaction
+  AbortTransaction,
+
+}
+
+struct TransactionRequest {
+  id: usize,
+  stock_id: usize,
+  price: usize,
+  quantity: usize
+}
+
+struct StockRequest {
+  stock_id: usize,
+  quantity: usize,
+}
