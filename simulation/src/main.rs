0dfbdb56334099469994262cbeee0660befd1d70
Kyle Dooley <dooleykh@rose-hulman.edu>
2015-02-15 19:36:25 -0500

    Moved enums and stocks to another file

--- simulation/src/main.rs
+++ simulation/src/main.rs
@@ -1,39 +1,7 @@
+pub mod messages;
+use messages::*;
+
 fn main() {
-    println!("Hello, world!");
-}
-
-// Messages to a Market
-enum MarketMessages {
-  SellRequest(TransactionRequest),
-  BuyRequest(TransactionRequest),
-  Commit(usize), //Id of the actor
-  Cancel(usize), //Id of the actor
-
-  MatchRequest(TransactionRequest, TransactionRequest) // (Buyer's Request, Seller's Request)
-}
-
-// Messages from a Market to an Actor
-enum ActorMessages {
-  StockRequest(StockRequest),
-  MoneyRequest(usize), //The amount of money needed to buy the stock(s)
-  CommitTransaction(TransactionRequest), //The information related to the transaction
-  AbortTransaction,
-}
-
-// Messages from a Market to a Teller
-enum TellerMessages {
-  SellRequest(TransactionRequest),
-  BuyRequest(TransactionRequest),
-}
-
-struct TransactionRequest {
-  actor_id: usize, //Id of Actor initiating the request
-  stock_id: usize,
-  price: usize,
-  quantity: usize
-}
-
-struct StockRequest {
-  stock_id: usize,
-  quantity: usize,
+  let sr = StockRequest {stock_id: 0, quantity: 1};
+  println!("ID: {}, Quantity: {}", sr.stock_id, sr.quantity);
 }
---
+++ simulation/src/messages.rs
@@ -0,0 +1,35 @@
+// Messages to a Market
+pub enum MarketMessages {
+  SellRequest(TransactionRequest),
+  BuyRequest(TransactionRequest),
+  Commit(usize), //Id of the actor
+  Cancel(usize), //Id of the actor
+
+  MatchRequest(TransactionRequest, TransactionRequest) // (Buyer's Request, Seller's Request)
+}
+
+// Messages from a Market to an Actor
+pub enum ActorMessages {
+  StockRequest(StockRequest),
+  MoneyRequest(usize), //The amount of money needed to buy the stock(s)
+  CommitTransaction(TransactionRequest), //The information related to the transaction
+  AbortTransaction,
+}
+
+// Messages from a Market to a Teller
+pub enum TellerMessages {
+  SellRequest(TransactionRequest),
+  BuyRequest(TransactionRequest),
+}
+
+pub struct TransactionRequest {
+  pub actor_id: usize, //Id of Actor initiating the request
+  pub stock_id: usize,
+  pub price: usize,
+  pub quantity: usize
+}
+
+pub struct StockRequest {
+  pub stock_id: usize,
+  pub quantity: usize,
+}
